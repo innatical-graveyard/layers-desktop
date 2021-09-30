e35e7c53a05487ffe19e1b2d0f35ffb400f2f7a6
Lleyton Gray <lleyton@innatical.com>
2021-09-30 08:59:35 -0700

    Ship source maps

--- next.config.js
+++ next.config.js
@@ -3,10 +3,10 @@
 // https://nextjs.org/docs/api-reference/next.config.js/introduction
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-const { withSentryConfig } = require('@sentry/nextjs');
+const { withSentryConfig } = require("@sentry/nextjs");
 
 const moduleExports = {
-  // Your existing module.exports
+  productionBrowserSourceMaps: true,
 };
 
 const SentryWebpackPluginOptions = {
