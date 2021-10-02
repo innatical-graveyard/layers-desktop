04cd1f8f5ed3ff8f13dbc9c6b558bb1839547058
trickypr <trickypr@icloud.com>
2021-10-02 16:31:43 +1000

    🚨 Fix compiler error

--- pages/app/settings/user.tsx
+++ pages/app/settings/user.tsx
@@ -33,167 +33,174 @@ const User = () => {
     <NavigationStack
       sidebar={SettingsSidebar}
       backUrl="/app/settings"
-      mainContent={({ backButton: BackButton }) => (
-        <div className="p-3 sm:m-auto w-full sm:w-1/3">
-          <div className="flex mb-2">
-            <BackButton />
-            <h1 className="ml-2 font-bold text-2xl">Profile</h1>
-          </div>
-          <div className="bg-card dark:bg-card-dark rounded-xl p-5">
-            <Formik
-              initialValues={{
-                username: me.data.user.username,
-                avatar: me.data.user.avatar,
-                email: me.data.user.email,
-              }}
-              validationSchema={yup.object().shape({
-                email: yup.string().email("Invalid email"),
-                username: yup
-                  .string()
-                  .min(3, "Too short, username must be at least 3 characters.")
-                  .max(16, "Too long, username must be under 16 characters."),
-                avatar: yup.string().url("Invalid URL"),
-              })}
-              onSubmit={async (values, { setErrors }) => {
-                if (!me.data.ok) return;
-                const res = await update.mutateAsync({
-                  username:
-                    values.username !== me.data.user.username
-                      ? values.username
-                      : undefined,
-                  email:
-                    values.email !== me.data.user.email
-                      ? values.email
-                      : undefined,
-                  avatar:
-                    values.avatar !== me.data.user.avatar
-                      ? values.avatar
-                      : undefined,
-                });
+      mainContent={({ backButton: BackButton }) => {
+        if (!me.data || !me.data.ok) return <></>;
 
-                if (!res.ok) {
-                  switch (res.error) {
-                    case "UsernameTaken":
-                      return setErrors({
-                        username: "This username is already in use",
-                      });
-                    case "EmailInUse":
-                      return setErrors({
-                        email: "This email is already in use",
-                      });
-                    default:
-                      return;
+        return (
+          <div className="p-3 sm:m-auto w-full sm:w-1/3">
+            <div className="flex mb-2">
+              <BackButton />
+              <h1 className="ml-2 font-bold text-2xl">Profile</h1>
+            </div>
+            <div className="bg-card dark:bg-card-dark rounded-xl p-5">
+              <Formik
+                initialValues={{
+                  username: me.data.user.username,
+                  avatar: me.data.user.avatar,
+                  email: me.data.user.email,
+                }}
+                validationSchema={yup.object().shape({
+                  email: yup.string().email("Invalid email"),
+                  username: yup
+                    .string()
+                    .min(
+                      3,
+                      "Too short, username must be at least 3 characters."
+                    )
+                    .max(16, "Too long, username must be under 16 characters."),
+                  avatar: yup.string().url("Invalid URL"),
+                })}
+                onSubmit={async (values, { setErrors }) => {
+                  if (!me.data.ok) return;
+                  const res = await update.mutateAsync({
+                    username:
+                      values.username !== me.data.user.username
+                        ? values.username
+                        : undefined,
+                    email:
+                      values.email !== me.data.user.email
+                        ? values.email
+                        : undefined,
+                    avatar:
+                      values.avatar !== me.data.user.avatar
+                        ? values.avatar
+                        : undefined,
+                  });
+
+                  if (!res.ok) {
+                    switch (res.error) {
+                      case "UsernameTaken":
+                        return setErrors({
+                          username: "This username is already in use",
+                        });
+                      case "EmailInUse":
+                        return setErrors({
+                          email: "This email is already in use",
+                        });
+                      default:
+                        return;
+                    }
                   }
-                }
 
-                await utils.invalidateQuery(["users.me"]);
-              }}
-            >
-              {({ isSubmitting, setFieldValue, values }) => (
-                <Form className="flex flex-col gap-3">
-                  <label htmlFor="">Avatar</label>
-                  <div className="p-5 dark:bg-input-elements-dark rounded-lg flex items-center">
-                    {me.data.ok && (
-                      <img
-                        src={values.avatar}
-                        className="rounded-xl object-cover w-12 h-12"
+                  await utils.invalidateQuery(["users.me"]);
+                }}
+              >
+                {({ isSubmitting, setFieldValue, values }) => (
+                  <Form className="flex flex-col gap-3">
+                    <label htmlFor="">Avatar</label>
+                    <div className="p-5 dark:bg-input-elements-dark rounded-lg flex items-center">
+                      {me.data.ok && (
+                        <img
+                          src={values.avatar}
+                          className="rounded-xl object-cover w-12 h-12"
+                        />
+                      )}
+                      <div className="ml-2">
+                        <p className="text-sm">
+                          Recommended icon size is 100x100
+                        </p>
+                        <h6 className="text-md font-bold">
+                          Max image size is 16mb
+                        </h6>
+                      </div>
+
+                      <IconButton
+                        icon={faFileUpload}
+                        color="bg-inndigo"
+                        className="ml-auto w-12 h-12"
+                        onClick={() => input.current?.click()}
+                        disabled={isUploading}
                       />
-                    )}
-                    <div className="ml-2">
-                      <p className="text-sm">
-                        Recommended icon size is 100x100
-                      </p>
-                      <h6 className="text-md font-bold">
-                        Max image size is 16mb
-                      </h6>
+
+                      <input
+                        className="hidden"
+                        ref={input}
+                        type="file"
+                        accept=".jpg, .png, .jpeg, .gif"
+                        onChange={async (event) => {
+                          setIsUploading(true);
+                          if (
+                            !event.target.files ||
+                            event.target.files.length <= 0 ||
+                            event.target.files.length > 1
+                          )
+                            return;
+                          const image = event.target.files.item(0);
+                          if (!image) return;
+                          const formData = new FormData();
+                          formData.append("file", image);
+                          const res = await fetch(
+                            "https://api.cdn.isometric.chat/upload/avatar",
+                            {
+                              method: "POST",
+                              body: formData,
+                              headers: {
+                                Authorization: token!,
+                              },
+                            }
+                          );
+                          const json = await res.json();
+
+                          setFieldValue(
+                            "avatar",
+                            "https://layers.fra1.cdn.digitaloceanspaces.com/" +
+                              json.id
+                          );
+                        }}
+                      />
                     </div>
 
-                    <IconButton
-                      icon={faFileUpload}
+                    <label htmlFor="username">Username</label>
+                    <Field
+                      name="username"
+                      id="username"
+                      className="p-3 rounded-lg dark:bg-input-elements-dark"
+                    />
+                    <ErrorMessage
+                      name="username"
+                      component="p"
+                      className="text-red-500"
+                    />
+
+                    <label htmlFor="email">Email</label>
+                    <Field
+                      type="email"
+                      name="email"
+                      id="email"
+                      className="p-3 rounded-lg dark:bg-input-elements-dark"
+                    />
+                    <ErrorMessage
+                      name="email"
+                      component="p"
+                      className="text-red-500"
+                    />
+
+                    <Button
+                      type="submit"
+                      disabled={isSubmitting}
                       color="bg-inndigo"
-                      className="ml-auto w-12 h-12"
-                      onClick={() => input.current?.click()}
-                      disabled={isUploading}
-                    />
-
-                    <input
-                      className="hidden"
-                      ref={input}
-                      type="file"
-                      accept=".jpg, .png, .jpeg, .gif"
-                      onChange={async (event) => {
-                        setIsUploading(true);
-                        if (
-                          !event.target.files ||
-                          event.target.files.length <= 0 ||
-                          event.target.files.length > 1
-                        )
-                          return;
-                        const image = event.target.files.item(0);
-                        if (!image) return;
-                        const formData = new FormData();
-                        formData.append("file", image);
-                        const res = await fetch(
-                          "https://api.cdn.isometric.chat/upload/avatar",
-                          {
-                            method: "POST",
-                            body: formData,
-                            headers: {
-                              Authorization: token!,
-                            },
-                          }
-                        );
-                        const json = await res.json();
-
-                        setFieldValue(
-                          "avatar",
-                          "https://layers.fra1.cdn.digitaloceanspaces.com/" +
-                            json.id
-                        );
-                      }}
-                    />
-                  </div>
-
-                  <label htmlFor="username">Username</label>
-                  <Field
-                    name="username"
-                    id="username"
-                    className="p-3 rounded-lg dark:bg-input-elements-dark"
-                  />
-                  <ErrorMessage
-                    name="username"
-                    component="p"
-                    className="text-red-500"
-                  />
-
-                  <label htmlFor="email">Email</label>
-                  <Field
-                    type="email"
-                    name="email"
-                    id="email"
-                    className="p-3 rounded-lg dark:bg-input-elements-dark"
-                  />
-                  <ErrorMessage
-                    name="email"
-                    component="p"
-                    className="text-red-500"
-                  />
-
-                  <Button
-                    type="submit"
-                    disabled={isSubmitting}
-                    color="bg-inndigo"
-                    className="rounded-lg mb-3"
-                    height="tall"
-                  >
-                    Save
-                  </Button>
-                </Form>
-              )}
-            </Formik>
+                      className="rounded-lg mb-3"
+                      height="tall"
+                    >
+                      Save
+                    </Button>
+                  </Form>
+                )}
+              </Formik>
+            </div>
           </div>
-        </div>
-      )}
+        );
+      }}
     />
   );
 };
